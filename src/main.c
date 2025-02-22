6c4e25ed2e7d52445029f5a7013e73f942c7f1bf
marian <marian.muller@serli.com>
2025-02-22 22:26:17 +0100

    windows position + scale

--- src/main.c
+++ src/main.c
@@ -436,7 +436,7 @@ int main(int argc, char** argv) {
     g_renderer_funcs = kSdlRendererFuncs;
   }
 
-  SDL_Window* window = SDL_CreateWindow(kWindowTitle, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, window_width, window_height, g_win_flags);
+  SDL_Window* window = SDL_CreateWindow(kWindowTitle, 0, 0, window_width, window_height, g_win_flags);
   if(window == NULL) {
     printf("Failed to create window: %s\n", SDL_GetError());
     return 1;
@@ -463,7 +463,7 @@ int main(int argc, char** argv) {
     g_dbg_renderer_funcs = kSdlDbgRendererFuncs;
   }
 
-  SDL_Window* dbg_window = SDL_CreateWindow(kDbgWindowTitle, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, dbg_window_width, dbg_window_height, g_dbg_win_flags);
+  SDL_Window* dbg_window = SDL_CreateWindow(kDbgWindowTitle, window_width, 0, dbg_window_width, dbg_window_height, g_dbg_win_flags);
   if(dbg_window == NULL) {
     printf("Failed to create dbg window: %s\n", SDL_GetError());
     return 1;
